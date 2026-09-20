3266fba3be439cd4e23e6b9c3479bf78527d2725
Lara Amr <laraamr1030@gmail.com>
2026-09-20 16:57:28 +0300

    Complete Day 2: Integrated BCM, inter-ECU auto-shifting logic, and clean repository state

--- build/src/powertrain/transmission_ecu/CMakeFiles/transmission_ecu.dir/main.cpp.o.d
+++ build/src/powertrain/transmission_ecu/CMakeFiles/transmission_ecu.dir/main.cpp.o.d
@@ -194,6 +194,4 @@ src/powertrain/transmission_ecu/CMakeFiles/transmission_ecu.dir/main.cpp.o: \
  /usr/include/c++/15/cstdint \
  /usr/lib/gcc/x86_64-linux-gnu/15/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
- /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
- /usr/include/c++/15/atomic /usr/include/c++/15/bits/atomic_base.h \
- /usr/include/c++/15/bits/atomic_lockfree_defines.h
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h
