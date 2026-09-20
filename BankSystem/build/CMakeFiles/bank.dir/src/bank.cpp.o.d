d1992d84ee8b91e5d10e9b810078df3707ee1ba9
Lara Amr <laraamr1030@gmail.com>
2026-09-20 16:00:15 +0300

    Remove unrelated BankSystem and EmployeeManagement folders

--- BankSystem/build/CMakeFiles/bank.dir/src/bank.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/bank.dir/src/bank.cpp.o: \
- /home/lara/automotive-ecu-project/BankSystem/src/bank.cpp \
- /usr/include/stdc-predef.h \
- /home/lara/automotive-ecu-project/BankSystem/include/bank.h
--- BankSystem/include/bank.h
+++
@@ -1,9 +0,0 @@
-#ifndef BANK_H
-#define BANK_H
-
-void setInitialBalance(double amount);
-void deposit(double amount);
-bool withdraw(double amount);
-double getBalance();
-
-#endif
